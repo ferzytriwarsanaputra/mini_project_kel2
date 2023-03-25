18a18095c5539bb76af2807cce0223c388cd4cea
Sopia Refaldi <112482825+Srefaldi@users.noreply.github.com>
2023-03-26 01:58:45 +0800

    Push Halaman Home dan style belum mvc

---
+++ views/home.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Home</title>
+</head>
+
+<body>
+
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>Ren</span>tal <span>Mo</span>bil</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+
+            <li><a href="login.php">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Mau Anter Ayang KKN? <span></span></h1>
+        <h1>Rental Mobil <span></span></h1>
+        <h1>Solusinya <span></span></h1>
+      </div>
+    </div>
+  </section>
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Ab<span>o</span>ut</h1>
+        <p>Rental mobil adalah layanan yang menyediakan kendaraan roda empat untuk disewakan sesuai dengan kebutuhan pengguna.
+          Rental mobil biasanya menyediakan berbagai jenis kendaraan, seperti mobil keluarga, sedan, SUV, van, dan lain-lain.
+          Layanan ini sangat berguna bagi mereka yang membutuhkan kendaraan untuk jangka waktu tertentu,
+          misalnya saat berlibur atau melakukan perjalanan bisnis. </p>
+        <p><b>Beberapa Mobil Yang Kami Tawarkan</b></p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <img src="img/mobil1.png">
+          <h2>Grand New Avanza 2019</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil2.png">
+          <h2>Toyota Fortuner 2.8 GR Sport</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil4.png">
+          <h2>Toyota Rush</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil3.png">
+          <h2>Honda Brio Satya</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil5.png">
+          <h2>New Pajero Sport</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil6.png">
+          <h2>Civic Hatchback RS </h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil7.png">
+          <h2>Suzuki Ignis </h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <img src="img/mobil8.png">
+          <h2>Honda HRV </h2>
+          <p></p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  </div>
+  </section>
+</body>
+
+</html>
